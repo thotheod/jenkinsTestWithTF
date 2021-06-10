094fbbd77bc21eab2ec1976979a86d0c771a9e83
Thodoris Theodorou <thotheod@microsoft.com>
2021-06-10 16:39:12 +0300

    test3

--- main.tf
+++ main.tf
@@ -33,7 +33,7 @@ resource "azurerm_resource_group" "rg" {
 
 resource "azurerm_storage_account" "example" {
   name                     = "storageaccountname"
-  resource_group_name      = "rg-${var.prefix}TF"
+  resource_group_name      = azurerm_resource_group.rg.name
   location                 = var.location
   account_tier             = "Standard"
   account_replication_type = "GRS"
