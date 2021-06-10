c018f4349606a73c46da316eeaaafb40b8a113c1
Thodoris Theodorou <thotheod@microsoft.com>
2021-06-10 16:34:12 +0300

    test 2

--- main.tf
+++ main.tf
@@ -30,3 +30,15 @@ resource "azurerm_resource_group" "rg" {
   location = var.location
   tags     = var.tags
 }
+
+resource "azurerm_storage_account" "example" {
+  name                     = "storageaccountname"
+  resource_group_name      = "rg-${var.prefix}TF"
+  location                 = var.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+
+  tags = {
+    environment = "testing"
+  }
+}
